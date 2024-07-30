6181fbc51dfe65870259d93e37ff4942e1bdcbf6
Arrebol <2362552845@qq.com>
2024-07-30 21:35:11 +0800

    feat: 开发查询商品分类信息接口，并存入到redis中

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -1,6 +1,5 @@
 package com.hmdp.controller;
 
-
 import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.service.IShopTypeService;
@@ -11,14 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
-/**
- * <p>
- * 前端控制器
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
 @RestController
 @RequestMapping("/shop-type")
 public class ShopTypeController {
@@ -27,8 +18,6 @@ public class ShopTypeController {
 
     @GetMapping("list")
     public Result queryTypeList() {
-        List<ShopType> typeList = typeService
-                .query().orderByAsc("sort").list();
-        return Result.ok(typeList);
+        return typeService.queryType();
     }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,20 +1,40 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
+
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryType() {
+        String cacheShopType = stringRedisTemplate.opsForValue().get(CACHE_SHOP_TYPE_KEY);
+        if (StrUtil.isNotBlank(cacheShopType)) {
+            List<ShopType> list = JSONUtil.toList(cacheShopType, ShopType.class);
+            return Result.ok(list);
+        }
+        List<ShopType> shopTypeList = list();
+        if (CollUtil.isEmpty(shopTypeList)) {
+            return Result.fail("未找到商品分类");
+        }
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_TYPE_KEY, JSONUtil.toJsonStr(shopTypeList));
+        return Result.ok(shopTypeList);
+    }
 }
