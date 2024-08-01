5bde73c57b518930a00977f56d2877d1c163435a
Arrebol <2362552845@qq.com>
2024-08-01 19:38:23 +0800

    feat: 通过互斥锁解决缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
@@ -26,30 +27,68 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
+        // 缓存穿透
+        // Shop shop = queryByCachePenetrate(id);
+
+        // 互斥锁  缓存击穿
+        Shop shop = queryByMutex(id);
+
+        return Result.ok(shop);
+    }
+
+    private Shop queryByMutex(Long id) {
         // 通过redis查询数据
         String cacheShop = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        if (StrUtil.isNotBlank(cacheShop)) {
+            return JSONUtil.toBean(cacheShop, Shop.class);
+        }
+        Shop shop = null;
+        // 尝试获取锁
+        boolean flag = tryLock(id);
+        try {
+            // 没有获取锁
+            if (!flag) {
+                Thread.sleep(50);
+                return queryByMutex(id);
+            }
+            // 有获取锁
+            // 通过redis查询数据
+            cacheShop = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+            if (StrUtil.isNotBlank(cacheShop)) {
+                return JSONUtil.toBean(cacheShop, Shop.class);
+            }
+            shop = getById(id);
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            unlock(id);
+        }
+        return shop;
+    }
 
+    private Shop queryByCachePenetrate(Long id) {
+        // 通过redis查询数据
+        String cacheShop = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
         if (StrUtil.isNotBlank(cacheShop)) {
             Shop shop = JSONUtil.toBean(cacheShop, Shop.class);
-            return Result.ok(shop);
+            return shop;
         }
-
         if (cacheShop != null) {
-            return Result.fail("店铺信息不存在");
+            //return Result.fail("店铺信息不存在");
+            return null;
         }
-
         Shop shop = getById(id);
-
         if (ObjectUtil.isNull(shop)) {
             // 如果redis中没有数据，但是数据库也没有数据，为了防止缓存穿透，将空值写入redis
             stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
-            return Result.fail("该店铺不存在");
+            //return Result.fail("该店铺不存在");
+            return null;
         }
 
         // 写入redis
         stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
-
-        return Result.ok(shop);
+        return shop;
     }
 
     @Override
@@ -64,5 +103,16 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         // 删除redis中的数据
         stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
         return Result.ok();
+    }
+
+    // 加锁
+    private boolean tryLock(Long id) {
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(LOCK_SHOP_KEY + id, String.valueOf(id), LOCK_SHOP_TTL, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    // 解锁
+    private void unlock(Long id) {
+        stringRedisTemplate.delete(LOCK_SHOP_KEY + id);
     }
 }
